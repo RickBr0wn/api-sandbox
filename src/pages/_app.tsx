91ab56310ba4639479b3d758242e2a7e5b310a44
Rick Brown <ricky.brown.00@gmail.com>
2023-03-13 11:21:20 +0000

    sets up prisma & next-auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,80 @@
-import '~/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { SessionProvider } from 'next-auth/react'
+import type { AppType } from 'next/app'
+import HeadLayout from '~/components/head-layout'
+import { Session } from 'next-auth'
+import type { NextPage } from 'next'
+// import type { GetStaticProps } from 'next'
+// import type { GetServerSideProps } from 'next'
+// import type { GetStaticPaths } from 'next'
+// import type { GetInitialProps } from 'next'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+interface AppProps {}
+
+const App: AppType<{ session: Session | null }> = ({
+	Component,
+	pageProps: { session, ...pageProps }
+}) => {
+	return (
+		<SessionProvider session={session}>
+			<HeadLayout>
+				<Component {...pageProps} />
+			</HeadLayout>
+		</SessionProvider>
+	)
 }
+
+/**
+ * @param context https://nextjs.org/docs/api-reference/data-fetching/get-static-props#context-parameter
+ * @return https://nextjs.org/docs/api-reference/data-fetching/get-static-props#getstaticprops-return-values
+ */
+// export const getStaticProps: GetStaticProps<AppProps> = async context => {
+//   // ...asyncronous code
+//   return {
+//     props: {} // will be passed to the page component as props
+//   }
+// }
+
+/**
+ * @param context https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props#context-parameter
+ * @return https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props#getserversideprops-return-values
+ */
+// export const getServerSideProps: GetServerSideProps = async context => {
+//   // ...asyncronous code
+//   return {
+//     props: {} // will be passed to the page component as props
+//   }
+// }
+
+/**
+ * @return https://nextjs.org/docs/api-reference/data-fetching/get-server-side-props#getserversideprops-return-values
+ */
+// export const getStaticPaths: GetStaticPaths = async () => {
+//   // ...asyncronous code
+//   return {
+//     paths: [
+//       {
+//         params: {
+//           /* ...params */
+//         }
+//       } // https://nextjs.org/docs/api-reference/data-fetching/get-static-paths#paths
+//     ],
+//     fallback: true // true, false or 'blocking' // https://nextjs.org/docs/api-reference/data-fetching/get-static-paths#fallback-false
+//   }
+// }
+
+/**
+ * @param context https://nextjs.org/docs/api-reference/data-fetching/get-initial-props#context-object
+ */
+// App.getInitialProps = async context => {
+//   // ...asyncronous code
+//   return {
+//     props: {} // will be passed to the page component as props
+//   }
+// }
+
+export default App
+
+// Path: src/pages/_app.tsx
+// Created at: 10:52:26 - 13/03/2023
+// Language: Typescript
+// Framework: React/Next.js
---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,31 @@
+import NextAuth, { Session, User } from 'next-auth'
+import DiscordProvider from 'next-auth/providers/discord'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../lib/prismadb'
+import { AdapterUser } from 'next-auth/adapters'
+import { JWT } from 'next-auth/jwt'
+
+export const authOptions = {
+	adapter: PrismaAdapter(prisma),
+	providers: [
+		DiscordProvider({
+			clientId: process.env.DISCORD_CLIENT_ID || '',
+			clientSecret: process.env.DISCORD_CLIENT_SECRET || ''
+		})
+	],
+	callbacks: {
+		async session({
+			session,
+			user,
+			token
+		}: {
+			session: Session
+			user: User | AdapterUser
+			token: JWT
+		}) {
+			return session
+		}
+	}
+}
+
+export default NextAuth(authOptions)
